58f6a308ae82f43bd16d7cc7838ffbe8c204678d
Jawad656567 <ja289327@gmail.com>
2025-09-20 11:33:51 +0500

    updates

--- components/SeachBar.jsx
+++ components/SeachBar.jsx
@@ -1,6 +1,6 @@
 export default function SearchBar({setQuery}) {
   return (
-
+// search
       <div className="search-container">
         <i className="fa-solid fa-magnifying-glass"></i>
         <input type="text" placeholder="Search for a country..."  onChange={(e)=>setQuery(e.target.value.toLowerCase())}  />
